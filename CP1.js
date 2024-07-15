e5f49694f1cad0e137c48dc0ca1cfe089ee54408
sahar <stambouli.sahar@gmail.com>
2024-07-15 15:57:52 +0100

    serverl fix

--- CP1.js
+++ CP1.js
@@ -2,26 +2,24 @@ console.log("Hello Wold")
 
 var str = prompt ("write a sentence ")
 
-
-
-
+console.log('str',str)
 // Nombre de caractères / nbre de mpts 
 
 nbwords = 0
-length= 0 
+nbcaractere= 0 
 voyels = 'aeiouAEIOU' 
 nbvoyels = 0 
 for (let i = 0; i < str.length; i++) {
     if (str[i] === ' ' || str[i] === '.') {
         nbwords = nbwords + 1;
     }
-    else {
-         length = length + 1 
+    
+    nbcaractere= nbcaractere + 1 
 
-    }
+    
 }
 
-console.log( 'nombre de caractères', length)
+console.log( 'nombre de caractères', nbcaractere)
 console.log('nombre de mots ',nbwords)
 
 //The number of vowels in the sentence.
